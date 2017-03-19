7892a98300d79517162740490226e2b44f4090df
Grinil Khanna <gmkhanna@gmail.com>
2017-03-18 19:05:24 -0700

    Style: css, colors, display of info

--- js/docAPI-interface.js
+++ js/docAPI-interface.js
@@ -1,21 +1,26 @@
 // Required items here (if applicable)
 var Doctors = require("./../js/docAPI.js").DoctorModule;
 
-//variables
-// var showDoctors = function (doctor) {
-//     $('#doc-info').append("<li>" + doctor.profile.first_name + "</li>");
-// };
-
-// console.log(showDoctors());
+// function headLoop() {
+//     for (var i = 0; i < headArray.length; i++)
+//     $('#ad').fadeIn(headArray[i], 2000);
+//     if (headArray[i] = headArray.length - 1) {
+//         headArray[i] = 0;
+//     }
+// }
 
 //Ready the Docum.
 $(document).ready(function() {
+    $("#caring").delay(1000).fadeIn(3000);
+    $("#exp").delay(3000).fadeIn(4000);
+    $("#here").delay(5000).fadeIn(4500);
     var DocList = new Doctors();
     $('#medIssue-search').submit(function(event) {
         event.preventDefault();
         var medCond = $('#medIssue').val();
         $('#medIssue').val("");
         DocList.nearby(medCond);
+
     });
 
 });
--- js/docAPI.js
+++ js/docAPI.js
@@ -6,34 +6,33 @@ var showDoctors;
 function Doctors() {
 
 }
-//
-// function searchFunc(medCond) {
-//     var splitBio = bio.split(' ');
-//     var count = 0;
-//
-//     for (var i = 0; i < splitBio.length; i++)
-//     if (medCond = splitBio[i])
-//     {
-//         count++;
-//     }
-//
-// }
 
+
+var nameList = [];
+var specList = [];
+var pracList = [];
 
 Doctors.prototype.nearby = function(medicalIssue) {
     $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + medicalIssue + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + apiKey)
     .then(function(response) {
         var dataArray = response.data;
         var amountDoctors = dataArray.length;
-        $('#amount-doctors').text("Doctors total in your area: " + amountDoctors);
+        $('#amount-doctors').text(amountDoctors);
 
         for (var i = 0; i < dataArray.length; i++) {
-            $('#name').text(dataArray[i].profile.first_name + " " + dataArray[i].profile.last_name + " " + dataArray[i].profile.title + "<br>" + dataArray[i].profile.bio);
+            nameList.push(dataArray[i].profile.first_name + " " + dataArray[i].profile.last_name + " " + dataArray[i].profile.title);
+            specList.push(dataArray[i].specialties[0].description);
+            pracList.push(dataArray[i].specialties[0].name);
+        }
+        for (var i = 0; i < nameList.length; i++) {
+            $('.doc-row').append();
+            $('.name').append("<h3>" + nameList[i] + "</h3>");
+            $('.bio').append("<ul>" + "<li>" + "Practice: " + pracList[i] + " - " + specList[i] + "</li>");
+            // $('.spec').append("<p>" + specList[i] + "</p>");
         }
     })
     .fail(function(error) {
-        $(".doc-display-area").text("failed!");
-        console.log("in fail of API");
+        $(".doc-display-area").append(error.responseJSON.message);
     });
 };
 
@@ -41,23 +40,5 @@ Doctors.prototype.nearby = function(medicalIssue) {
 exports.DoctorModule = Doctors;
 exports.nearby = Doctors.nearby;
 exports.doctorCount = doctorCount;
-
-// var docArray = [];
-// var doctorCount = Object.keys(response.data).length;
-//
-// response.data.forEach(function(doctor) {
-//     showDoctors(medCond);
-// });
-// // docArray.push(response.data)
-//
-// console.log(dataArray);
-// for (var i = 0; i < amountDoctors; i++)
-// var indivDoc = dataArray[i];
-// for (var key in indivDoc) {
-//     var docInfo = key;
-//     var docInfoObj = indivDoc[key];
-//     // console.log(label);
-//     console.log(docInfoObj);
-// $('#doc-info').append("<li>" + "lets see " + docInfo + "</li>");
-//
-// }
+exports.nameList = nameList;
+exports.specList = specList;
